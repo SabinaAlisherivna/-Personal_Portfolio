60e9c5b0752fbb467a6a3d5cd5ff827a210e9635
sabina <UZBprogrammer30@gmail.com>
2024-10-29 13:05:51 +0500

    add Left-Side List +Typing Effect+Mobile View+overlay+new img

--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 
 let MenuBtn = document.getElementById('MenuBtn')
 
-MenuBtn.addEventListener('click',function(e)) {
- document.querySelector('body').classList.toggle('mobile-nav-active')
+MenuBtn.addEventListener('click', function(e) {
+   
+  document.querySelector('body').classList.toggle('mobile-nav-active');
  this.classList.toggle('fa-xmark')
-}
+})
+
+//Typing Effect
+let typed = new Typed('.auto-input',{
+ strings:['Front-End Developer!','Frelancer!','JS Developer'],
+ typedSpeed:100,
+ backSpeed:100,
+ backDelay:2000,
+ loop:true,
+})
